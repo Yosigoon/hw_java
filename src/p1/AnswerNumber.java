7f1240a0693305b07409eebb5cb4c3e05880b99f
bit <bit@bitcamp>
2017-07-13 16:16:27 +0900

    7/13

--- src/p1/AnswerNumber.java
+++ src/p1/AnswerNumber.java
@@ -7,17 +7,18 @@ public class AnswerNumber {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int current = 100 >> 1;
+		int max = 100;
+		int current = max >> 1;
 //		System.out.println("현재 값은 " + current);
-		int idx = 1;
 		
-		outer:
+		
+		
 		for (;;) {
 			System.out.println("생각하는 숫자가 " + current + "보다 어때? (h or l or c)");
 			String answer = sc.next();
 			switch (answer) {
 			case "h":
-//				current =
+//				current = 
 				break;
 			case "l":
 //				current =
